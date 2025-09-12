ef60743dbc8e29e57602376e806d0ba1c4d80c2a
NeRrO106 <andreilimit@yahoo.com>
2025-09-12 15:46:25 +0300

    Updated App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </BrowserRouter>

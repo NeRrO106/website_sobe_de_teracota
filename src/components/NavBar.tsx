07e2ce0e16d5038dc0e76c1d7e007bebf57170c3
NeRrO106 <andreilimit@yahoo.com>
2025-09-12 15:08:02 +0300

    Created navabr component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <header className="absolute inset-x-0 top-0 z-50 bg-amber-500">
+      <nav
+        className="flex items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <Link to="/" className="-m-1.5 p-1.5 text-white text-lg font-bold">
+            <span>Atelier Sobe</span>
+          </Link>
+        </div>
+
+        <div className="hidden lg:flex lg:gap-x-12">
+          <Link to="#produse" className="text-sm font-semibold text-white">
+            Produse
+          </Link>
+          <Link to="#despre" className="text-sm font-semibold text-white">
+            Despre noi
+          </Link>
+          <Link to="#contact" className="text-sm font-semibold text-white">
+            Contact
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
